1f33e1a62bc15dc51ec21ebe15755dc5c83fb3e0
DyeslenSilva <dyeslensilva97@gmail.com>
2021-06-06 21:18:53 -0300

    alterando componentes..

---
+++ src/main/java/br/com/zup/others/ConsultaDeVeiculos.java
@@ -0,0 +1,5 @@
+package br.com.zup.others;
+
+public class ConsultaDeVeiculos {
+
+}
--- src/main/java/br/com/zup/repo/UsuarioVeiculoRepo.java
+++
@@ -1,9 +0,0 @@
-package br.com.zup.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.zup.model.VeiculoUsuario;
-
-public interface UsuarioVeiculoRepo extends JpaRepository<VeiculoUsuario, String> {
-
-}
