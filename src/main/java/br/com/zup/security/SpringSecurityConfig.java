e18197870aa6626e9a78f5560dcadda1f87da670
DyeslenSilva <dyeslensilva97@gmail.com>
2021-06-10 20:59:38 -0300

    alterando componentes...

--- src/main/java/br/com/zup/security/SpringSecurityConfig.java
+++
@@ -1,8 +0,0 @@
-package br.com.zup.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityConfig extends AbstractSecurityWebApplicationInitializer {
-	
-
-}
